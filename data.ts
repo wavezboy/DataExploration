7d7fdb08da4276e0e791254eef5161ce1466e9ff
wavezboy <sanusifatah16@gmail.com>
2023-12-25 15:37:25 +0100

    oo

--- data.ts
+++ data.ts
@@ -11,25 +11,23 @@ const users = [
   { id: 9, name: "Klein" },
 ];
 
-
 const friendships = [
-    [0, 1],
-    [0, 2],
-    [1, 2],
-    [1, 3],
-    [2, 3],
-    [3, 4],
-    [4, 5],
-    [5, 6],
-    [6, 7],
-    [7, 8],
-    [8, 9]
-  ];
-  
-  // Each element in the 'friendships' array is now an array representing an edge.
-  // For example, [0, 1] represents an edge between node 0 and node 1.
-  
+  [0, 1],
+  [0, 2],
+  [1, 2],
+  [1, 3],
+  [2, 3],
+  [3, 4],
+  [4, 5],
+  [5, 6],
+  [6, 7],
+  [7, 8],
+  [8, 9],
+];
 
-for(let i =0; i<users.length; i++){
-    users[i].
+// Each element in the 'friendships' array is now an array representing an edge.
+// For example, [0, 1] represents an edge between node 0 and node 1.
+
+for (const user of users) {
+  user["friends"] = [];
 }
